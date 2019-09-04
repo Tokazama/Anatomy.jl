876f89da5a45b7815c4e9e005ca834f2ceca6b92
Tokazama <zchristensen7@gmail.com>
2019-09-04 13:18:54 -0400

    More synapse

--- src/synapse.jl
+++ src/synapse.jl
@@ -1,8 +1,85 @@
+###
+### Synapse interface
+###
 """
-    AbstractSynapse
+    AbstractSynapse{Pre,Pos,AP,A}
 
 """
-abstract type AbstractSynapse{Pre<:AbstractAnatomy,Pos<:AbstractAnatomy} end
+abstract type AbstractSynapse{Pre,Pos,AP,A} end
+
+"""
+    Synapse
+"""
+struct Synapse{Pre,Pos,AP,A} <: AbstractSynapse{Pre,Pos,AP,A}
+    presynaptic::Pre
+    postsynaptic::Pos
+    activityparams::AP
+    activity::A
+end
 
 
-abstract type FiberBundle{N,T} <: AbstractVector{T} end
+"""
+    presynaptic(s::AbstractSynapse)
+"""
+function presynaptic(s::AbstractSynapse) end
+presynaptic(s::Synapse{Pre,Pos,AP,A}) where {Pre,Pos,AP,A} = getproperty(s, :presynaptic)::Pre
+
+"""
+    postsynaptic(s::AbstractSynapse)
+"""
+function postsynaptic(s::AbstractSynapse) end
+postsynaptic(s::Synapse{Pre,Pos,AP,A}) where {Pre,Pos,AP,A} = getproperty(s, :postsynaptic)::Pos
+
+"""
+    activityparams(s::AbstractSynapse)
+"""
+function activityparams(s::AbstractSynapse) end
+activityparams(s::Synapse{Pre,Pos,AP,A}) where {Pre,Pos,AP,A} = getproperty(s, :activityparams)::AP
+
+"""
+    activity(::AbstractSynapse)
+"""
+function activity(s::AbstractSynapse) end
+activity(s::Synapse{Pre,Pos,AP,A}) where {Pre,Pos,AP,A} = getproperty(s, :activity)::A
+
+"""
+    activate!(s::AbstractSynapse)
+"""
+activate!(s::AbstractSynapse) = activity(s)(presynaptic(s), postsynaptic(s), activityparams(s))
+
+
+###
+### Fiber interface
+###
+
+"""
+    AbstractFiberBundle
+
+"""
+abstract type AbstractFiberBundle{N,T} <: AbstractVector{T} end
+
+"""
+    FiberBundle
+
+
+
+struct LineString{
+        Dim, T <: Real,
+        P <: AbstractPoint,
+        V <: AbstractVector{<: LineP{Dim, T, P}}
+    } <: AbstractVector{LineP{Dim, T, P}}
+    points::V
+    path::LineSegments
+end
+
+"""
+struct FiberBundle{D,T,P,V}
+    pathway::LineString{D,T,P,V}
+end
+
+merge_fiber_segments()
+
+function mergefibers
+end
+
+
