3e411358f70bc3f44675e520bf8d6afa2ae31e99
Zachary Christensen <zchristensen@nsc-foxemacpro.urmc-sh.rochester.edu>
2019-09-04 15:49:01 -0400

    Adding fiber bundles

--- src/synapse.jl
+++ src/synapse.jl
@@ -1,3 +1,7 @@
+# TODO:
+# mergefiber - this should probably have interpolative, averaging, and fiber cut options/variations
+# combinefibers - ditto
+
 ###
 ### Synapse interface
 ###
@@ -56,30 +60,51 @@ activate!(s::AbstractSynapse) = activity(s)(presynaptic(s), postsynaptic(s), act
     AbstractFiberBundle
 
 """
-abstract type AbstractFiberBundle{N,T} <: AbstractVector{T} end
+abstract type AbstractFiberBundle{Nu,Syn,T,P} <: AbstractVector{T} end
 
 """
     FiberBundle
 
-
-
-struct LineString{
-        Dim, T <: Real,
-        P <: AbstractPoint,
-        V <: AbstractVector{<: LineP{Dim, T, P}}
-    } <: AbstractVector{LineP{Dim, T, P}}
-    points::V
-    path::LineSegments
+"""
+struct FiberBundle{Nu,Syn,T,P<:AbstractVector{T}} <: AbstractFiberBundle{Nu,Syn,T,P}
+    nucleus::Nu
+    synapse::Syn
+    pathway::P
 end
 
 """
-struct FiberBundle{D,T,P,V}
-    pathway::LineString{D,T,P,V}
-end
+    nucleus(f::AbstractFiberBundle) -> AbstractAnatomy
+"""
+function nucleus(f::AbstractFiberBundle) end
+nucleus(f::FiberBundle{Nu,Syn,T,P}) where {Nu,Syn,T,P} = getproperty(f, :nucleus)::Nu
 
-merge_fiber_segments()
+"""
+    synapse(f::AbstractFiberBundle) -> AbstractSynapse
+"""
+function synapse(f::AbstractFiberBundle) end
+synapse(f::FiberBundle{Nu,Syn,T,P}) where {Nu,Syn,T,P} = getproperty(f, :synapse)::Syn
 
-function mergefibers
-end
+"""
+    pathway(f::AbstractFiberBundle) -> AbstractSynapse
+"""
+function pathway(f::AbstractFiberBundle) end
+pathway(f::FiberBundle{Nu,Syn,T,P}) where {Nu,Syn,T,P} = getproperty(f, :pathway)::P
+
+
+
+
+"""
+    mergefibers(method, f; kwargs...)
+
+Merge fiber bundle segments along its pathway.
+"""
+function mergefiber(method, f::FiberBundle; kwargs...) end
+
+"""
+    combinefibers(method, f...; kwargs)
+
+Merge fiber bundles together into a single fiber bundle.
+"""
+function combinefibers(method, f::FiberBundle...; kwargs...) end
 
 
